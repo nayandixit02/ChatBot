0a62d10ea12fdedcb2592efac72fcf9cd3c28c4d
Nayan Dixit <nayandixit1503@gmail.com>
2025-04-25 00:52:06 +0530

    some changes in package.json

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,11 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import path from "path";
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: path.resolve(__dirname, "../backend/public"),
-    emptyOutDir: true,
-  },
 });
