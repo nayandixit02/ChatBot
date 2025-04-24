6dd216b2f90454d0585efc5fbbfa48e5dc069415
Nayan Dixit <nayandixit1503@gmail.com>
2025-04-25 02:09:02 +0530

    final changes to link frontend and backend

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -10,7 +10,13 @@ config();
 const app = express();
 
 //middlewares
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+// app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(
+  cors({
+    origin: "https://your-frontend-site.onrender.com",
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(cookieParser(process.env.COOKIE_SECRET));
 
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -7,7 +7,8 @@ import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext.tsx";
 import { Toaster } from "react-hot-toast";
 import axios from "axios";
-axios.defaults.baseURL = "http://localhost:5000/api/v1";
+// axios.defaults.baseURL = "http://localhost:5000/api/v1";
+axios.defaults.baseURL = import.meta.env.VITE_API_BASE_URL;
 axios.defaults.withCredentials = true;
 
 const theme = createTheme({
