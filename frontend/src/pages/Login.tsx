25cf4416bf5ac7171adcce9febdc9fb48a5c67e0
Nayan Dixit <nayandixit1503@gmail.com>
2025-06-08 23:27:50 +0530

    some changes in login.tsx

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { IoIosLogIn } from "react-icons/io";
 import { Box, Typography, Button } from "@mui/material";
 import CustomizedInput from "../components/shared/CustomizedInput";
@@ -8,34 +8,15 @@ import { useNavigate } from "react-router-dom";
 const Login = () => {
   const navigate = useNavigate();
   const auth = useAuth();
-  // const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-  //   e.preventDefault();
-  //   const formData = new FormData(e.currentTarget);
-  //   const email = formData.get("email") as string;
-  //   const password = formData.get("password") as string;
-  //   try {
-  //     toast.loading("Signing In", { id: "login" });
-  //     await auth?.login(email, password);
-  //     toast.success("Signed In Successfully", { id: "login" });
-  //   } catch (error) {
-  //     console.log(error);
-  //     toast.error("Signing In Failed", { id: "login" });
-  //   }
-  // };
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     const formData = new FormData(e.currentTarget);
     const email = formData.get("email") as string;
     const password = formData.get("password") as string;
-
     try {
       toast.loading("Signing In", { id: "login" });
       await auth?.login(email, password);
       toast.success("Signed In Successfully", { id: "login" });
-
-      setTimeout(() => {
-        navigate("/chat");
-      }, 300);
     } catch (error) {
       console.log(error);
       toast.error("Signing In Failed", { id: "login" });
@@ -46,6 +27,12 @@ const Login = () => {
   //     navigate("/chat");
   //   }
   // }, [auth, navigate]);
+  useEffect(() => {
+    if (auth?.user) {
+      navigate("/chat");
+    }
+  }, [auth?.user, navigate]);
+
   return (
     <Box width={"100%"} height={"100%"} display="flex" flex={1}>
       <Box padding={8} mt={8} display={{ md: "flex", sm: "none", xs: "none" }}>
